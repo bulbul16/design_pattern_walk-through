bd4d57786f1fdb260f2d107701da17ab7662ac8e
bulbul <bulbul.cse@outlook.com>
2023-02-23 17:56:50 +0600

    Initial Commit

---
+++ SimpleFactory/models/CarFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory.models
+{
+	public class CarFactory
+	{
+		public Car GetCar(string carType = "FourSeater")
+		{
+			Car car = null;
+
+			switch (carType)
+			{
+				case "FourSeater":
+					car = new FourSeaterCar();
+					break;
+				case "SixSeater":
+					car = new SixSeaterCar();
+					break;
+			}
+			car.Manufacture();
+			return car;
+
+		}
+	}
+}
